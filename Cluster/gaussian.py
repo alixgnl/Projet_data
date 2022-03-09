c4ebfd491ae89237b65f469a2c7cc033d430e1c7
AntoineJu34 <81319341+AntoineJu34@users.noreply.github.com>
2022-03-09 14:41:38 +0100

    création d'une méthode Gaussian mixture model

---
+++ Cluster/gaussian.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+data = pd.read_csv('C:\\Users\\junda\\Downloads\\Clustering_gmm.csv')
+"""
+plt.figure(figsize=(7,7))
+plt.scatter(data["Weight"],data["Height"])
+plt.xlabel('Weight')
+plt.ylabel('Height')
+plt.title('Data Distribution')
+plt.show()
+"""
+# training gaussian mixture model 
+from sklearn.mixture import GaussianMixture
+gmm = GaussianMixture(n_components=4)
+gmm.fit(data)
+
+#predictions from gmm
+labels = gmm.predict(data)
+frame = pd.DataFrame(data)
+frame['cluster'] = labels
+frame.columns = ['Weight', 'Height', 'cluster']
+
+color=['blue','green','cyan', 'black']
+for k in range(0,4):
+    data = frame[frame["cluster"]==k]
+    plt.scatter(data["Weight"],data["Height"],c=color[k])
+plt.show()
--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print ("hello")
+from sklearn import mixture
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu_0 = 5.0
+srd_0 = 2.0
+
+data = np.random.randn(100000)
+data = data * srd_0 + mu_0
+
+data = data.reshape(-1, 1)
+
+hx, hy, _ = plt.hist(data, bins=50, density=1,color="lightblue")
+
+plt.ylim(0.0,max(hx)+0.05)
+plt.title('Gaussian mixture example 01')
+plt.grid()
+
+plt.xlim(mu_0-4*srd_0,mu_0+4*srd_0)
+
+plt.savefig("example_gmm_01.png", bbox_inches='tight')
+plt.show()
+
+gmm = mixture.GaussianMixture(n_components=1, covariance_type='full').fit(data)
+
+print(gmm.means_)
+print(np.sqrt(gmm.covariances_))
+
+[[5.00715457]]
+[[[1.99746652]]]
