d29730fb1f41ec5d8ed5e96551c780c546b95707
AntoineJu34 <81319341+AntoineJu34@users.noreply.github.com>
2022-03-25 12:17:43 +0100

    mis à jour

--- Cluster/gaussian.py
+++ Cluster/gaussian.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import numpy
+import scipy.io
 import csv
 import matplotlib.pyplot as plt
 data = pd.read_csv('C:\\Users\\junda\\Downloads\\Clustering_gmm.csv')
-"""
+
 plt.figure(figsize=(7,7))
 plt.scatter(data["Weight"],data["Height"])
 plt.xlabel('Weight')
 plt.ylabel('Height')
 plt.title('Data Distribution')
 plt.show()
-"""
+
 # training gaussian mixture model 
 from sklearn.mixture import GaussianMixture
 gmm = GaussianMixture(n_components=4)
