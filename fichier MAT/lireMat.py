698e04a90f12fe66b1706bc1d2731cc41d02d5d0
AntoineJu34 <81319341+AntoineJu34@users.noreply.github.com>
2022-03-25 15:22:00 +0100

    suppression fichier

--- fichier MAT/lireMat.py
+++
@@ -1,7 +0,0 @@
-import scipy.io
-import numpy as np
-mat = scipy.io.loadmat ("C:\\Users\\junda\\OneDrive\\Bureau\\breastw.mat")
-print(type(mat))
-
-X = mat['X'] #On extrait la donnée du champ 'X' du dictionnaire
-(np.shape(X)) #shape() de la bibliothèque numpy (ici np), renvoie la taille de la matrice (array numpy) en argument
