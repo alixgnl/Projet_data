395dd8d798f17f3c0fa2cd216f71732bec336516
AntoineJu34 <81319341+AntoineJu34@users.noreply.github.com>
2022-03-31 17:30:41 +0200

    Méthode Gaussian Mixture models

--- Cluster/gaussian.py
+++ Cluster/gaussian.py
@@ -5,6 +5,8 @@ import csv
 import matplotlib.pyplot as plt
 data = pd.read_csv('C:\\Users\\junda\\Downloads\\Clustering_gmm.csv')
 
+
+
 plt.figure(figsize=(7,7))
 plt.scatter(data["Weight"],data["Height"])
 plt.xlabel('Weight')
